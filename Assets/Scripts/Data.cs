fb4a08201dd4d7ba8513a627f3fd4ea713b0bc4a
pollyaana <148092515+pollyaana@users.noreply.github.com>
2024-09-08 15:39:50 +0300

    Update Data.cs

--- Assets/Scripts/Data.cs
+++ Assets/Scripts/Data.cs
@@ -1,5 +1,5 @@
 public class Data
 {
-    public static string supabaseUrl = "https://vtdyecqfthdiegrqssbw.supabase.co";
-    public  static string supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0ZHllY3FmdGhkaWVncnFzc2J3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMzE2ODI5OCwiZXhwIjoyMDI4NzQ0Mjk4fQ.Uovfe2NpVq1eg3xzQmUR111ImRJMZWIzABFyQwqeKxY";
+    public static string supabaseUrl = "";
+    public  static string supabaseKey = "";
 }
